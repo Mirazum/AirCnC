a2b13d9bd977977684a57839dab4cfcda92fc459
Mirazum Parvin <mirapar@digitalentshelsinki.internal>
2023-05-26 14:41:05 +0300

    first commit

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCf-_CrmvmB6FCMmPH7sXA8wmHy33ttH1g",
+  authDomain: "airconic-4c51e.firebaseapp.com",
+  projectId: "airconic-4c51e",
+  storageBucket: "airconic-4c51e.appspot.com",
+  messagingSenderId: "194114800430",
+  appId: "1:194114800430:web:53074af7da28a61097c8bf"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
