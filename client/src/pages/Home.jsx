8c07b98d6ee73eb615fc1284f567d22a4292b20c
Mirazum <mira.sust@gmail.com>
2023-05-29 13:47:49 +0300

    categorybox  done

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Categories from '../Components/Categories/categories';
+
+const Home = () => {
+    return (
+        <>
+           <Categories></Categories>
+        </>
+    );
+};
+
+export default Home;
